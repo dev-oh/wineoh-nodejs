cea8e396d1e2125cc56d35b566da6c7fd6a8de56
Raheel <dt.raheel@gmail.com>
2018-04-05 16:01:07 +0530

    Mod

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -243,8 +243,7 @@ module.exports = {
                     LeadSource: 'Website',
                     uid__c: response.uid,
                     // StatisPerson__c : 'STAGED'
-            }
-                ;
+                };
                 var uid = shortId.generate();
                 var promise = [
                     Contact.findOne({Email: req.body.email}),
@@ -319,11 +318,8 @@ module.exports = {
                                             return res.badRequest(error);
                                         }
                                         sails.log.info('existing record updated');
-                                        sails.log.info('Calling Segment');
-                                        SegmentService.identifyTrait(uid, user);
-                                        SegmentService.track(uid, user.Email, 'Lead Updated');
                                         sails.log.info('creating firebase user');
-                                        Lead.update({Email: user.Email}, user).then(updatedLead => {
+                                        Lead.update({Email: user.Email}, 444).then(updatedLead => {
                                             sails.log.info('postgre lead updated');
                                             res.ok(updatedLead);
                                         });
@@ -362,9 +358,139 @@ module.exports = {
                             })
                         }
                     }))
-
             }).catch(error => {
             return res.ok('Error occure while trying to authenticate', 'Authentication Error', 'FAIL')
         })
     },
+    passwordLessRegister: (req, res) => {
+        console.log(req.body);
+        var promise = []
+        var user = {
+            LastName: req.body.lastName,
+            FirstName: req.body.firstName,
+            RecordTypeId: '01228000000SbEwAAK',
+            Company: req.body.company,
+            Email: req.body.email,
+            Website: req.body.domain,
+            Status: 'Interest',
+            ART__c: req.body.type,
+            LeadSource: 'Website',
+        }
+        var promises = [
+            Contact.findOne({Email: req.body.email}),
+            Lead.findOne({Email: req.body.email})
+        ];
+        var uid = shortId.generate();
+        Promise.all(promises)
+            .then(_.spread((contact, lead) => {
+                if (contact) {
+                    sails.log.info('contact found');
+                    if (contact.StatusPerson__c === 'UNPROVISIONED') {
+                        sails.log.info('creating firebase account');
+                        sails.log.info('updating sfdc lead');
+                        conn.sobject('Lead').update(user, (error, updatedRecord) => {
+                            if (error || !updatedRecord.success)
+                                sails.log.info("Not able")
+                            return res.serverError("Unable To Bind Firebase Id With Account");
+                            sails.log.info('sfdc lead updated');
+                            console.log({updatedRecord: updatedRecord});
+                            sails.log.info('updating postgre lead');
+                            Lead.update({Email: user.Email}, user).then(updatedLead => {
+                                sails.log.info('postgre lead updated');
+                                res.ok(updatedLead);
+                            });
+                        })
+                    } else if (
+                        contact.StatusPerson__c === 'STAGED' ||
+                        contact.StatusPerson__c === 'RECOVERY' ||
+                        contact.StatusPerson__c === 'LOCKED_OUT' ||
+                        contact.StatusPerson__c === 'ACTIVE'
+                    ) {
+                        // logic here
+                        res.ok('goSignIn');
+                    } else if (
+                        contact.StatusPerson__c === 'PROVISIONED' ||
+                        contact.StatusPerson__c === 'PW_EXPIRED'
+                    ) {
+                        res.ok('sendPassword')
+                    } else if (
+                        contact.StatusPerson__c === 'SUSPENDED' ||
+                        contact.StatusPerson__c === 'DEPROVISIONED'
+                    ) {
+                        res.ok('contactSupport');
+                    }
+                }
+                else if (lead) {
+                    sails.log.info("lead found");
+                    sails.log.info('checking if account exist');
+                    if (lead.uid__c) {
+                        sails.log.info('account already exist');
+                        return res.ok('alreadyExist');
+                    }
+                    sails.log.info('account not exist');
+                    sails.log.info('creating sfdc connection');
+                    conn.login(salsForceConfig.username, salsForceConfig.password, function (err, resp) {
+                        sails.log.info('sfdc connection established');
+                        lead = _.pickBy(lead, _.identity);
+                        sails.log.info('selecting data from sfdc');
+                        conn.sobject('Lead').find({Email: lead.Email}).execute((error, record) => {
+                            if (error) return res.badRequest(error);
+                            sails.log.info('data fetched from sfdc');
+                            lead.Id = record[0].Id;
+                            user.Id = record[0].Id;
+                            delete lead.LastModifiedDate;
+                            delete lead.Name;
+                            delete lead.SystemModstamp;
+                            delete lead.CreatedDate;
+                            sails.log.info('updating existing record');
+                            conn.sobject('Lead').update(user, (error, updatedRecord) => {
+                                if (error) {
+                                    sails.log.info('unable to update record');
+                                    return res.badRequest(error);
+                                }
+                                sails.log.info('existing record updated');
+                                sails.log.info('Calling Segment');
+                                SegmentService.identifyTrait(uid, user);
+                                SegmentService.track(uid, user.Email, 'Lead Updated');
+                                sails.log.info('creating firebase user');
+                                Lead.update({Email: user.Email}, user).then(updatedLead => {
+                                    sails.log.info('postgre lead updated');
+                                    res.ok(updatedLead);
+                                });
+                            });
+                        });
+                    });
+
+                }
+                else {
+                    sails.log.info("no contact or lead");
+                    sails.log.info('signing in to sfdc');
+                    conn.login(salsForceConfig.username, salsForceConfig.password, function (err, resp) {
+                        sails.log.info('signed in to sfdc');
+                        sails.log.info('inserting data to "Lead" table');
+                        conn.sobject('Lead').create(user, (error, createdUser) => {
+                            if (error) {
+                                console.log(error)
+                                return res.badRequest(error);
+                            }
+                            sails.log.info('data inserted into "Lead"');
+                            user.Id = createdUser.id;
+                            sails.log.info('Calling Segment');
+                            // SegmentService.identifyTrait(uid, user);
+                            // SegmentService.track(uid, user.Email, 'Lead Added');
+                            sails.log.info('Inserting data into PostgreSQL (Lead)');
+                            Lead.create(user).then(createdLead => {
+                                sails.log.info('data inserted into PostgreSQL');
+                                sails.log.info('creating firebase user');
+                                res.ok(createdLead)
+                            }).catch(error => {
+                                sails.log.info("unable to insert lead into postgre");
+                                console.log(error);
+                                res.ok("An error occur while creating Account", 'Internal Server Error', 'FAIL');
+                            });
+                        });
+                    })
+                }
+            }))
+    }
 };
